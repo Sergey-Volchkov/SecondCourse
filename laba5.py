f1f9da45a20d106b56569c340f3a10475b1b2144
Sergey-Volchkov <seriyvlastelin@gmail.com>
2018-05-06 21:09:48 +0300

    Update laba5.py

--- laba5.py
+++ laba5.py
@@ -35,19 +35,20 @@ def parsing_of_massives(dict_buf,list_buf):
         masXspam.append(xspam_dict[element]/dict_buf[element])
     return masKey,masValue,masXspam
 def drawing_of_graphics(masKey,masValue,masXspam):
-    print(masKey[1],masXspam[1])
+    fig = plt.figure(figsize=(10,5))
+
     plt.title('Количество писем')
-    # plt.bar(masKey,masValue, align='center')
-    plt.bar(masKey,masValue,1, align='center')
-    plt.xticks(fontsize='6', rotation=90)
-    plt.savefig("1.png",figsize=(10, 6))
+    i = np.arange(len(masValue))
+    plt.bar(i,masValue,width=0.5)
+    plt.xticks(i,masKey,fontsize='6', rotation='vertical')
+    plt.savefig("1.png")
+    plt.subplots_adjust(bottom=0.4)
     plt.show()
-
+    i = np.arange(len(masXspam))
     plt.title('Средний показатель спама по каждому отправителю')
-    plt.bar(masKey,masXspam, align='center')
-    plt.xticks(fontsize='6', rotation=90)
-    plt.savefig("2.png",figsize=(10, 6))
-
+    plt.bar(i,masXspam,width=0.5)
+    plt.xticks(i,masKey,fontsize='6', rotation='vertical')
+    plt.savefig("2.png")
     plt.show()
 def main():
     masFrom, xspam_dict = parsing_of_file()
